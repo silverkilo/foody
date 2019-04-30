5925c7b3a201e5cce42d46e014118271c475f322
Omar Juice <ojameer1@gmail.com>
2019-04-30 16:26:15 -0400

    initial commit

---
+++ server/index.js
@@ -0,0 +1,26 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const app = express()
+const PORT = process.env.PORT || 3001
+
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+
+
+
+app.use(express.static(path.join(__dirname, '..', 'build')))
+
+app.get('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'build/index.html'))
+})
+app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send(err.message || 'INTERNAL SERVER ERROR')
+})
+
+app.listen(PORT, () => {
+    console.log(`LISTENING ON PORT ${PORT}`)
+})
