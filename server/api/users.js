a5cf385bc81e0a23b8f980f429588ba6a6f0456c
Jason Levine <jasonclevine@me.com>
2019-04-30 19:38:45 -0400

    get userId route, post user route

--- server/api/users.js
+++ server/api/users.js
@@ -1,15 +1,33 @@
 const router = require('express').Router()
 const {User} = require('../db/models')
+module.exports = router
 
-router.get('/:userId', async (req, res, next) {
-    try {
-        const userId = await Number(req.params.userId)
-        const user = await User.findByPk(userId)
-        res.json({
-            id: user.id,
-            email: user.email,
-        })
-    } catch (err) {
-        next(err)
-    }
+router.get('/:userId', async (req, res, next) => {
+  try {
+    const userId = await Number(req.params.userId)
+    const user = await User.findByPk(userId)
+    res.json({
+      id: user.id,
+      firstName: user.firstName,
+      lastName: user.lastName,
+      email: user.email
+    })
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newUser = await User.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      googleId: null,
+      password: req.body.password
+    })
+    res.status(201).send(newUser)
+  } catch (err) {
+    next(err)
+  }
 })
--- server/index.js
+++ server/index.js
@@ -5,25 +5,23 @@ const db = require('./db')
 const app = express()
 const PORT = process.env.PORT || 3001
 
-
 app.use(morgan('dev'))
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
-
+app.use('/api', require('./api'))
 
 app.use(express.static(path.join(__dirname, '..', 'build')))
 
 app.get('*', (req, res, next) => {
-    res.sendFile(path.join(__dirname, '..', 'build/index.html'))
+  res.sendFile(path.join(__dirname, '..', 'build/index.html'))
 })
 app.use((err, req, res, next) => {
-    res.status(err.status || 500)
-    res.send(err.message || 'INTERNAL SERVER ERROR')
+  res.status(err.status || 500)
+  res.send(err.message || 'INTERNAL SERVER ERROR')
 })
 db.sync().then(function() {
-    app.listen(PORT, () => {
-        console.log(`LISTENING ON PORT ${PORT}`)
-    })
+  app.listen(PORT, () => {
+    console.log(`LISTENING ON PORT ${PORT}`)
+  })
 })
-
