b2001f636af2f2058c4e4a87ef5c70d08d2cdcf8
Omar Juice <ojameer1@gmail.com>
2019-05-01 12:50:29 -0400

    category routes

--- server/index.js
+++ server/index.js
@@ -6,21 +6,23 @@ const app = express()
 const PORT = process.env.PORT || 3001
 
 app.use(morgan('dev'))
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
 app.use('/api', require('./api'))
 
 app.use(express.static(path.join(__dirname, '..', 'build')))
 
-app.get('*', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'build/index.html'))
-})
-app.use((err, req, res, next) => {
-  res.status(err.status || 500)
-  res.send(err.message || 'INTERNAL SERVER ERROR')
-})
-db.sync().then(function() {
+if (process.env.NODE_ENV === 'production') {
+  app.get('*', (req, res, next) => {
+    res.sendFile(path.join(__dirname, '..', 'build/index.html'))
+  })
+  app.use((err, req, res, next) => {
+    res.status(err.status || 500)
+    res.send(err.message || 'INTERNAL SERVER ERROR')
+  })
+}
+db.sync().then(function () {
   app.listen(PORT, () => {
     console.log(`LISTENING ON PORT ${PORT}`)
   })
