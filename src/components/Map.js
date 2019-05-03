e5b9c2ce20827594414e596dc78294a90c16153c
Sasha Kayola <ok515@nyu.edu>
2019-05-03 17:20:02 -0400

    added a dummy marker over the map

--- src/components/Map.js
+++ src/components/Map.js
@@ -1,19 +1,31 @@
 import {Component} from 'react';
-import MapGL from 'react-map-gl';
+import MapGL, {Marker} from 'react-map-gl';
 import React from 'react'
+import './mapstyles.css'
+
+function randomIcon(){
+  return Math.floor(Math.random() * 8) + 1;
+}
 
 export class Map extends Component {
   constructor(){
   super()
   this.state = {
     viewport: {
-      width: 400,
-      height: 400,
-      latitude: 37.7577,
-      longitude: -122.4376,
-      zoom: 8
+      width: 375,
+      height: 812,
+      latitude: 40.7128,
+      longitude: -74.0060,
+      zoom: 12
+    },
+    icon: randomIcon()
     }
-    }
+  }
+
+  componentDidMount() {
+    this.setState({
+      icon: randomIcon()
+    })
   }
 
   render(){
@@ -23,7 +35,11 @@ export class Map extends Component {
         mapStyle='mapbox://styles/rhearao/cjv749h8x1o4f1fpff04veqj9'
         onViewportChange={(viewport) => this.setState({viewport})}
         mapboxApiAccessToken={'pk.eyJ1IjoicmhlYXJhbyIsImEiOiJjanQ0ajJ3MjUwMjJrNDlvM2ExcmszcXZ3In0.xztopoCKZUlUCYgWMy7Djw'}
-      />
+        >
+        <Marker latitude={40.7128} longitude={-74.0060} offsetLeft={-20} offsetTop={-10}>
+          <div className={`marker marker${this.state.icon}`}></div>
+        </Marker>
+      </MapGL>
   )}
 }
 
