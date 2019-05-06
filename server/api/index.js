65f77d99b25bc9e460bf4636883cefc6cb620a65
Omar Juice <ojameer1@gmail.com>
2019-05-06 12:31:58 -0400

    matches routes setup

--- server/api/index.js
+++ server/api/index.js
@@ -5,6 +5,7 @@ router.use('/users', require('./users'))
 router.use('/categories', require('./categories'))
 router.use('/preferences', require('./preferences'))
 router.use('/locations', require('./locations'))
+router.use('/matches', require('./matches'))
 router.use((req, res, next) => {
   const error = new Error('Not Found')
   error.status = 404
---
+++ server/api/matches.js
@@ -0,0 +1,4 @@
+const router = require('express').Router()
+const { Preference, User, UserPreference, Location, UserLocation } = require('../db/models')
+module.exports = router
+
