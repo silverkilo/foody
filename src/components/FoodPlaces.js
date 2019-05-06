6f324f0a572c883f21b7277617e9b1138546abf5
Sasha Kayola <ok515@nyu.edu>
2019-05-06 12:25:50 -0400

    added food places component

---
+++ src/components/FoodPlaces.js
@@ -0,0 +1,89 @@
+import {Component} from 'react';
+import MapGL, {Marker} from 'react-map-gl';
+import React from 'react'
+import './mapstyles.css'
+
+function randomIcon(){
+  return Math.floor(Math.random() * 8) + 1;
+}
+
+export class FoodPlaces extends Component {
+  constructor(){
+  super()
+  this.state = {
+    viewport: {
+      width: 375,
+      height: 812,
+      latitude: 40.7128,
+      longitude: -74.0060,
+      zoom: 14
+    },
+    icon: randomIcon(),
+    lat: 40.7128,
+    long: -74.0060,
+    venues: []
+    }
+    this.test = this.test.bind(this)
+  }
+
+  componentDidMount() {
+    this.setState({
+      icon: randomIcon()
+    })
+
+  }
+
+  getVenues() {
+    let setVenueState = this.setState.bind(this);
+    const venuesEndpoint = 'https://api.foursquare.com/v2/venues/explore?';
+
+    const params = {
+      client_id: 'ZJWIIRNTTOXTXGYZCHG3YPBEUHSCWKNE00T3LBIE15FXARUB',
+      client_secret: 'KBJ5SOQRK3LL3LO1SPEMO4XWYOU0TVRE2XEPL5G2P1V44QXS',
+      limit: 100,
+      query: 'Food',
+      v: '20130619', // version of the API
+      ll: `${this.state.lat}, ${this.state.long}`
+    };
+
+    fetch(venuesEndpoint + new URLSearchParams(params), {
+      method: 'GET'
+    }).then(response => response.json()).then(response => {
+      setVenueState({venues: response.response.groups[0].items});
+    });
+  }
+
+  test(position) {
+    let lat = position.coords.latitude
+    let long = position.coords.longitude
+    this.setState({
+      viewport: {...this.state.viewport, latitude: lat, longitude: long},
+      lat: lat,
+      long: long
+    })
+  }
+
+  render(){
+    window.navigator.geolocation.getCurrentPosition(this.test)
+    this.getVenues();
+    return (
+      <MapGL
+        {...this.state.viewport}
+        mapStyle='mapbox://styles/rhearao/cjv749h8x1o4f1fpff04veqj9'
+        onViewportChange={(viewport) => this.setState({viewport})}
+        mapboxApiAccessToken={'pk.eyJ1IjoicmhlYXJhbyIsImEiOiJjanQ0ajJ3MjUwMjJrNDlvM2ExcmszcXZ3In0.xztopoCKZUlUCYgWMy7Djw'}
+      >
+        <Marker latitude={this.state.lat} longitude={this.state.long} offsetLeft={-20} offsetTop={-10}>
+          <div className={`marker marker${this.state.icon}`}></div>
+        </Marker>
+
+        {this.state.venues.map(item =>
+          <Marker latitude={item.venue.location.lat} longitude={item.venue.location.lng} offsetLeft={-20} offsetTop={-10}>
+          {/* <div>{item.venue.name}</div> */}
+          <div className={`foodMarker food`}></div>
+        </Marker>
+        )}
+
+      </MapGL>
+  )}
+}
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react'
 import {connect} from 'react-redux'
 import {withRouter, Route, Switch} from 'react-router-dom'
-import {AuthForm, UserProfile, UpdateUser, Preference, Matching, Map} from './components'
+import {AuthForm, UserProfile, UpdateUser, Preference, Matching, Map, FoodPlaces} from './components'
 import {me} from './store'
 
 class Routes extends Component {
@@ -19,6 +19,7 @@ class Routes extends Component {
         <Route path='/preference' component={Preference} />
         <Route path='/matches' component={Matching} />
         <Route path='/map' component={Map} />
+        <Route path='/food' component={FoodPlaces} />
 
       </Switch>
     )
