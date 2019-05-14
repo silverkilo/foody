9e02e56aa8d1c2ff512b23f1962b1cdae3289e1c
Jason Levine <jasonclevine@me.com>
2019-05-14 13:16:38 -0400

    initial card style

--- src/components/MatchCard.js
+++ src/components/MatchCard.js
@@ -8,20 +8,47 @@ export default function MatchCard({
   distance,
   preferences
 }) {
+  console.log(
+    "id",
+    id,
+    "firstName",
+    firstName,
+    "lastName",
+    lastName,
+    "photoURLs",
+    photoURLs,
+    "distance",
+    distance,
+    "preferences",
+    preferences
+  );
   return (
-    <div>
-      {/* displaying id for debugging purposes only */}
-
-      <h1 className="match-name">
-        {id}: {firstName} {lastName}
-      </h1>
-
-      <div className="match-image">
-        <img src={photoURLs[0]} alt={firstName} />
+    <div className="match__card">
+      <img className="match__img" src={photoURLs[0]} alt="" />
+      <div className="match__details">
+        <h1 className="match__name">
+          {firstName} {lastName}
+        </h1>
+        <ul className="match__preferences">
+          {preferences.map(preference => (
+            <li className="match__preference">{preference}</li>
+          ))}
+        </ul>
       </div>
-      {/* match distance will be deleted, for debugging purposes only */}
-      <strong className="match-distance">{distance}</strong>
-      <p className="match-prefs">{preferences.join(", ")}</p>
     </div>
+    // <div>
+    //   {/* displaying id for debugging purposes only */}
+
+    //   <h1 className="match-name">
+    //     {id}: {firstName} {lastName}
+    //   </h1>
+
+    //   <div className="match-image">
+    //     <img src={photoURLs[0]} alt={firstName} />
+    //   </div>
+    //   {/* match distance will be deleted, for debugging purposes only */}
+    //   <strong className="match-distance">{distance}</strong>
+    //   <p className="match-prefs">{preferences.join(", ")}</p>
+    // </div>
   );
 }
