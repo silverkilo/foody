d8e7a39eb5e2041a6116cbda610f252471a80263
Rhea Rao <imrhearao@gmail.com>
2019-05-15 18:10:30 -0400

    matching window modal

--- src/components/Matching.js
+++ src/components/Matching.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import MatchStack from "./MatchStack";
 import { swipe } from "../store";
+import ReactModal from "react-modal";
 import NewMatch from "./NewMatch";
 import NoMatches from "./NoMatches";
 import Nav from "./Nav";
@@ -24,10 +25,10 @@ class Matching extends Component {
     }
   }
   render() {
-    if (this.props.didMatch.matched) {
-      const match = this.props.didMatch.info;
-      return <NewMatch {...match} />;
-    }
+    // if (this.props.didMatch.matched) {
+    //   const match = this.props.didMatch.info;
+    //   return <NewMatch {...match} />;
+    // }
     if (!this.props.potentials.length) return <NoMatches />;
     const users = this.props.potentials;
 
@@ -37,6 +38,39 @@ class Matching extends Component {
         <div className="match-container">
           {this.props.loading ? null : <MatchStack users={users} />}
         </div>
+        <ReactModal
+          isOpen={this.props.didMatch.matched ? true : false}
+          shouldCloseOnOverlayClick={true}
+          closeTimeoutMS={5000}
+          contentLabel="Restaurant Selected Modal"
+          className="congrats__content"
+          overlayClassName="congrats__overlay"
+          // style={{ overlay: {}, content: "hi is this working" }}
+          // portalClassName="ReactModalPortal"
+          // overlayClassName="ReactModal__Overlay"
+          // className="ReactModal__Content"
+          // bodyOpenClassName="ReactModal__Body--open"
+          // htmlOpenClassName="ReactModal__Html--open"
+          // ariaHideApp={true}
+          // role="dialog"
+          // parentSelector={() => document.body}
+          // data={{
+          //   background: "blue"
+          // }}
+        >
+          <i className="fas fa-utensils congrats__icon" />
+          <h1 className="congrats__title"> Congratulations! </h1>{" "}
+          <div key={this.props.didMatch.info.id}>
+            <p className="congrats__text">
+              You have matched with {this.props.didMatch.info.firstName}{" "}
+              {this.props.didMatch.info.lastName}
+            </p>
+            <img
+              src={this.props.didMatch.info.photoURLs[0]}
+              alt={this.props.didMatch.info.firstName}
+            />
+          </div>
+        </ReactModal>
       </React.Fragment>
     );
   }
