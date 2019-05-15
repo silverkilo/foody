8e78abfd6ee4cc5e0da798560909e717bfdf270b
Sasha Kayola <ok515@nyu.edu>
2019-05-15 15:47:04 -0400

    awaiting food places after getting preferences

--- src/components/FoodDetails.js
+++ src/components/FoodDetails.js
@@ -1,6 +1,13 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
-import { select, deselect } from "../store/food";
+import React, {
+  Component
+} from "react";
+import {
+  connect
+} from "react-redux";
+import {
+  select,
+  deselect
+} from "../store/food";
 import t from "typy";
 import axios from "axios";
 
@@ -28,8 +35,8 @@ class FoodDetails extends Component {
   getVenuesDetails = async () => {
     const venueId = this.props.venueId;
     const params = {
-      client_id: "KUZ0H02M1VQNYUNKV40GFCICQUYGHRZJQVFLFS4MK01IHFYE",
-      client_secret: "ESQTWW5FJSPUDTTCM5JWQ1EO3T1GXNRVMS5XTKR3AKC4GNVJ",
+      client_id: "5DQ4HC1WROBOH0SFRD4IULDTPLLRP4J5LWKMOG0SZ0LRV5K0",
+      client_secret: "E5PLXEXQKZMQMPU02YDTSV0I1ZIAFK5LI0KPAEEZUCQQ5OJ3",
       v: "20130619"
     };
     const venuesEndpoint = `https://api.foursquare.com/v2/venues/${venueId}?&client_id=${
@@ -37,7 +44,9 @@ class FoodDetails extends Component {
     }&client_secret=${params.client_secret}&v=${params.v}`;
 
     const res = await axios.get(venuesEndpoint);
-    const { venue } = res.data.response;
+    const {
+      venue
+    } = res.data.response;
     console.log(venue);
     this.setState({
       name: t(venue, "name").safeObject,
@@ -68,15 +77,15 @@ class FoodDetails extends Component {
 
   createStars = () => {
     const rating = Math.round(this.state.rating / 2);
-    let stars = [
-      <i className="fas fa-star empty" />,
-      <i className="fas fa-star empty" />,
-      <i className="fas fa-star empty" />,
-      <i className="fas fa-star empty" />,
-      <i className="fas fa-star empty" />
+    let stars = [ <
+      i className = "fas fa-star empty" / > , <
+      i className = "fas fa-star empty" / > , <
+      i className = "fas fa-star empty" / > , <
+      i className = "fas fa-star empty" / > , <
+      i className = "fas fa-star empty" / >
     ];
     for (let i = 0; i < rating; i++) {
-      stars[i] = <i className="fas fa-star" />;
+      stars[i] = < i className = "fas fa-star" / > ;
     }
     return stars;
   };
@@ -92,48 +101,69 @@ class FoodDetails extends Component {
   };
 
   render() {
-    return (
-      <div className={this.state.card}>
-        {this.state.photo !== undefined ? (
-          <img
-            className="card__img"
-            src={this.state.photo.prefix + "200x200" + this.state.photo.suffix}
-            alt=""
-          />
-        ) : (
-          <img className="card__img" src="./images/stock.jpg" alt="" />
-        )}
-        <ul className="card__details">
-          <li className="card__name">{this.state.name}</li>
-          <li className="card__rating">{this.createStars()}</li>
-          <li className="card__price">
-            {this.createCurrency()} {this.state.category}
-          </li>
-          <li className="card__address">{this.state.address}</li>
-          <li className="card__address">
-            {this.state.city}, {this.state.state}
-          </li>
-        </ul>
-        {this.props.food.includes(this.props.venueId) ? (
-          <button
-            className="card__button"
-            onClick={() => {
+    return ( <
+      div className = {
+        this.state.card
+      } > {
+        this.state.photo !== undefined ? ( <
+          img className = "card__img"
+          src = {
+            this.state.photo.prefix + "200x200" + this.state.photo.suffix
+          }
+          alt = "" /
+          >
+        ) : ( <
+          img className = "card__img"
+          src = "./images/stock.jpg"
+          alt = "" / >
+        )
+      } <
+      ul className = "card__details" >
+      <
+      li className = "card__name" > {
+        this.state.name
+      } < /li> <
+      li className = "card__rating" > {
+        this.createStars()
+      } < /li> <
+      li className = "card__price" > {
+        this.createCurrency()
+      } {
+        this.state.category
+      } <
+      /li> <
+      li className = "card__address" > {
+        this.state.address
+      } < /li> <
+      li className = "card__address" > {
+        this.state.city
+      }, {
+        this.state.state
+      } <
+      /li> < /
+      ul > {
+        this.props.food.includes(this.props.venueId) ? ( <
+          button className = "card__button"
+          onClick = {
+            () => {
               this.handleDeselect(this.props.venueId);
-            }}
-          >
-            Remove
-          </button>
-        ) : (
-          <button
-            className="card__button"
-            onClick={() => {
+            }
+          } >
+          Remove <
+          /button>
+        ) : ( <
+          button className = "card__button"
+          onClick = {
+            () => {
               this.handleSelect(this.props.venueId);
-            }}
-          >
-            pick me!
-          </button>
-        )}
-      </div>
+            }
+          } >
+          pick me!
+          <
+          /button>
+        )
+      } <
+      /div>
     );
   }
 }
--- src/components/MapBox.js
+++ src/components/MapBox.js
@@ -1,15 +1,36 @@
-import React, { Component } from "react";
-import MapGL, { Marker } from "react-map-gl";
-import { Link } from "react-router-dom";
+import React, {
+  Component
+} from "react";
+import MapGL, {
+  Marker
+} from "react-map-gl";
+import {
+  Link
+} from "react-router-dom";
 import ReactModal from "react-modal";
 import SwipeLayer from "./SwipeLayer";
-import { connect } from "react-redux";
-import { setUserLocation, getMatchLocation } from "../store";
-import { setSelectedIdx } from "../store/highlight";
-import { getMatchPreference } from "../store/matchPreference";
-import { joinChatRoom } from "../store/chat";
-import { createVenueList } from "../store/food";
-import { setIconImg } from "../store/icon";
+import {
+  connect
+} from "react-redux";
+import {
+  setUserLocation,
+  getMatchLocation
+} from "../store";
+import {
+  setSelectedIdx
+} from "../store/highlight";
+import {
+  getMatchPreference
+} from "../store/matchPreference";
+import {
+  joinChatRoom
+} from "../store/chat";
+import {
+  createVenueList
+} from "../store/food";
+import {
+  setIconImg
+} from "../store/icon";
 import Chat from "./Chat";
 import "./mapstyles.css";
 import Nav from "./Nav";
@@ -54,8 +75,7 @@ export class MapBox extends Component {
       let lat = this.props.userLat;
       let long = this.props.userLong;
       resolve([long, lat]);
-      this.setState(
-        {
+      this.setState({
           viewport: {
             ...this.state.viewport,
             latitude: lat,
@@ -78,22 +98,20 @@ export class MapBox extends Component {
     let midpointLat = (lat + this.props.matchLat) / 2;
     let midpointLong = (long + this.props.matchLong) / 2;
 
-    await this.getVenues(
-      midpointLat,
-      midpointLong,
-      distance > 1000 ? distance : 1000
-    );
     this.props.joinChatRoom();
     this.props.setIconImg();
     this.props.createVenueList();
-    this.setState(
-      {
-        matchPreferences: this.props.matchInfo
-          ? this.props.matchInfo.preferences
-          : this.state.matchPreferences
+    this.setState({
+        matchPreferences: this.props.matchInfo ?
+          this.props.matchInfo.preferences : this.state.matchPreferences
       },
-      () => {
-        console.log(this.state);
+      async () => {
+        console.log("STATE AFTER SETTING MATCH PREFERENCES", this.state);
+        await this.getVenues(
+          midpointLat,
+          midpointLong,
+          distance > 1000 ? distance : 1000
+        );
       }
     );
   }
@@ -139,9 +157,9 @@ export class MapBox extends Component {
     const venuesEndpoint = "https://api.foursquare.com/v2/venues/search?";
 
     const params = {
-      client_id: "KUZ0H02M1VQNYUNKV40GFCICQUYGHRZJQVFLFS4MK01IHFYE",
-      client_secret: "ESQTWW5FJSPUDTTCM5JWQ1EO3T1GXNRVMS5XTKR3AKC4GNVJ",
-      limit: 5,
+      client_id: "5DQ4HC1WROBOH0SFRD4IULDTPLLRP4J5LWKMOG0SZ0LRV5K0",
+      client_secret: "E5PLXEXQKZMQMPU02YDTSV0I1ZIAFK5LI0KPAEEZUCQQ5OJ3",
+      limit: 30,
       query: "Food",
       v: "20130619", // version of the API
       ll: `${lat}, ${long}`,
@@ -149,27 +167,26 @@ export class MapBox extends Component {
     };
 
     await fetch(venuesEndpoint + new URLSearchParams(params), {
-      method: "GET"
-    })
+        method: "GET"
+      })
       .then(response => response.json())
       .then(response => {
         // filter out those places without category names
         let filteredWithoutCategories = response.response.venues.filter(
           eachPlace => eachPlace.categories[0] !== undefined
         );
+        console.log("filteredWithoutCategories", filteredWithoutCategories)
         // filter out places with categories that don't match the user's preferences
         let filtered = filteredWithoutCategories.filter(
           eachPlace =>
-            this.state.matchPreferences.indexOf(eachPlace.categories[0].name) >
-            -1
+          this.state.matchPreferences.indexOf(eachPlace.categories[0].name) >
+          -1
         );
         this.setState({
-          allVenues: filtered
+          allVenues: filtered,
+          loadedVenues: true
         });
       });
-    this.setState({
-      loadedVenues: true
-    });
   };
 
   //chat functions
@@ -183,15 +200,15 @@ export class MapBox extends Component {
   };
   createStars = () => {
     const rating = Math.round(this.state.selectedRestaurant.rating / 2);
-    let stars = [
-      <i className="fas fa-star empty" />,
-      <i className="fas fa-star empty" />,
-      <i className="fas fa-star empty" />,
-      <i className="fas fa-star empty" />,
-      <i className="fas fa-star empty" />
+    let stars = [ <
+      i className = "fas fa-star empty" / > , <
+      i className = "fas fa-star empty" / > , <
+      i className = "fas fa-star empty" / > , <
+      i className = "fas fa-star empty" / > , <
+      i className = "fas fa-star empty" / >
     ];
     for (let i = 0; i < rating; i++) {
-      stars[i] = <i className="fas fa-star" />;
+      stars[i] = < i className = "fas fa-star" / > ;
     }
     return stars;
   };
@@ -206,121 +223,196 @@ export class MapBox extends Component {
     return signs;
   };
   render() {
-    return (
-      <React.Fragment>
-        <Nav />
-        <div className="map">
-          <MapGL
-            {...this.state.viewport}
-            mapStyle="mapbox://styles/rhearao/cjve4ypqx3uct1fo7p0uyb5hu"
-            onViewportChange={viewport =>
-              this.setState({
-                viewport
-              })
+    return ( <
+        React.Fragment >
+        <
+        Nav / >
+        <
+        div className = "map" >
+        <
+        MapGL {
+          ...this.state.viewport
+        }
+        mapStyle = "mapbox://styles/rhearao/cjve4ypqx3uct1fo7p0uyb5hu"
+        onViewportChange = {
+          viewport =>
+          this.setState({
+            viewport
+          })
+        }
+        mapboxApiAccessToken = "pk.eyJ1Ijoib2theW9sYSIsImEiOiJjanY3MXZva2MwMnB2M3pudG0xcWhrcWN2In0.mBX1cWn8lOgPUD0LBXHkWg" >
+        <
+        Marker latitude = {
+          this.props.userLat
+        }
+        longitude = {
+          this.props.userLong
+        }
+        offsetLeft = {
+          -20
+        }
+        offsetTop = {
+          -10
+        } >
+        <
+        div className = {
+          `marker marker${this.props.icon1}`
+        }
+        /> < /
+        Marker > <
+        Marker latitude = {
+          this.props.matchLat
+        }
+        longitude = {
+          this.props.matchLong
+        }
+        offsetLeft = {
+          -20
+        }
+        offsetTop = {
+          -10
+        } >
+        <
+        div className = {
+          `marker marker${this.props.icon2}`
+        }
+        /> < /
+        Marker > {
+          this.state.allVenues.map((item, index) => {
+            let icon;
+            this.props.selectedIdx === index ?
+              (icon = `highlightedFooodMarker`) :
+              (icon = `foodMarker`);
+            return ( <
+              Marker latitude = {
+                item.location.lat
+              }
+              longitude = {
+                item.location.lng
+              }
+              offsetLeft = {
+                -20
+              }
+              offsetTop = {
+                -10
+              }
+              key = {
+                item.id
+              } >
+              <
+              div onClick = {
+                () => {
+                  this.props.setSelectedIdx(index);
+                }
+              }
+              className = {
+                icon
+              }
+              />{" "} < /
+              Marker >
+            );
+          })
+        } {
+          " "
+        } <
+        /MapGL>{" "} < /
+        div > {
+          " "
+        } <
+        button className = "chatBubble"
+        onClick = {
+          this.handleOpenChat
+        } >
+        <
+        i class = "fas fa-comment-alt" / >
+        <
+        /button> <
+        Chat / > {
+          this.state.loadedVenues && ( <
+            div className = "overlay" >
+            <
+            div className = "content" >
+            <
+            SwipeLayer allVenues = {
+              this.state.allVenues
             }
-            mapboxApiAccessToken="pk.eyJ1Ijoib2theW9sYSIsImEiOiJjanY3MXZva2MwMnB2M3pudG0xcWhrcWN2In0.mBX1cWn8lOgPUD0LBXHkWg"
-          >
-            <Marker
-              latitude={this.props.userLat}
-              longitude={this.props.userLong}
-              offsetLeft={-20}
-              offsetTop={-10}
-            >
-              <div className={`marker marker${this.props.icon1}`} />
-            </Marker>
-            <Marker
-              latitude={this.props.matchLat}
-              longitude={this.props.matchLong}
-              offsetLeft={-20}
-              offsetTop={-10}
-            >
-              <div className={`marker marker${this.props.icon2}`} />
-            </Marker>
-            {this.state.allVenues.map((item, index) => {
-              let icon;
-              this.props.selectedIdx === index
-                ? (icon = `highlightedFooodMarker`)
-                : (icon = `foodMarker`);
-              return (
-                <Marker
-                  latitude={item.location.lat}
-                  longitude={item.location.lng}
-                  offsetLeft={-20}
-                  offsetTop={-10}
-                  key={item.id}
-                >
-                  <div
-                    onClick={() => {
-                      this.props.setSelectedIdx(index);
-                    }}
-                    className={icon}
-                  />{" "}
-                </Marker>
-              );
-            })}{" "}
-          </MapGL>{" "}
-        </div>{" "}
-        <button className="chatBubble" onClick={this.handleOpenChat}>
-          <i class="fas fa-comment-alt" />
-        </button>
-        <Chat />
-        {this.state.loadedVenues && (
-          <div className="overlay">
-            <div className="content">
-              <SwipeLayer allVenues={this.state.allVenues} />{" "}
-            </div>{" "}
-          </div>
-        )}
-        <ReactModal
-          isOpen={this.props.selectedRestaurant ? true : false}
-          shouldCloseOnOverlayClick={true}
-          closeTimeoutMS={5000}
-          contentLabel="Restaurant Selected Modal"
-          className="congrats__content"
-          overlayClassName="congrats__overlay"
-          // style={{ overlay: {}, content: "hi is this working" }}
-          // portalClassName="ReactModalPortal"
-          // overlayClassName="ReactModal__Overlay"
-          // className="ReactModal__Content"
-          // bodyOpenClassName="ReactModal__Body--open"
-          // htmlOpenClassName="ReactModal__Html--open"
-          // ariaHideApp={true}
-          // role="dialog"
-          // parentSelector={() => document.body}
-          // data={{
-          //   background: "blue"
-          // }}
+            />{" "} < /
+            div > {
+              " "
+            } <
+            /div>
+          )
+        } <
+        ReactModal isOpen = {
+          this.props.selectedRestaurant ? true : false
+        }
+        shouldCloseOnOverlayClick = {
+          true
+        }
+        closeTimeoutMS = {
+          5000
+        }
+        contentLabel = "Restaurant Selected Modal"
+        className = "congrats__content"
+        overlayClassName = "congrats__overlay"
+        // style={{ overlay: {}, content: "hi is this working" }}
+        // portalClassName="ReactModalPortal"
+        // overlayClassName="ReactModal__Overlay"
+        // className="ReactModal__Content"
+        // bodyOpenClassName="ReactModal__Body--open"
+        // htmlOpenClassName="ReactModal__Html--open"
+        // ariaHideApp={true}
+        // role="dialog"
+        // parentSelector={() => document.body}
+        // data={{
+        //   background: "blue"
+        // }}
         >
-          <i className="fas fa-utensils congrats__icon" />
-          <h1 className="congrats__title">Congratulations!</h1>
-          <p className="congrats__text">
-            You have both selected {this.state.selectedRestaurant.name}
-          </p>
-          <span className="congrats__text">{this.createStars()}</span>
-          {this.createCurrency() !== "" ? (
-            <span className="congrats__text">{this.createCurrency()}</span>
-          ) : (
-            ""
-          )}
-          <span className="congrats__text">
-            {this.state.selectedRestaurant.address}
-          </span>
-          <span className="congrats__text">
-            {this.state.selectedRestaurant.city},{" "}
-            {this.state.selectedRestaurant.state}
-          </span>
-          <button
-            className="congrats__button"
-            onClick={() => {
-              this.handlePopupClose();
-            }}
-          >
-            Lets Go!
-          </button>
-        </ReactModal>
-      </React.Fragment>
-    );
-  }
+        <
+        i className = "fas fa-utensils congrats__icon" / >
+        <
+        h1 className = "congrats__title" > Congratulations! < /h1> <
+        p className = "congrats__text" >
+        You have both selected {
+          this.state.selectedRestaurant.name
+        } <
+        /p> <
+        span className = "congrats__text" > {
+          this.createStars()
+        } < /span> {
+        this.createCurrency() !== "" ? ( <
+          span className = "congrats__text" > {
+            this.createCurrency()
+          } < /span>
+        ) : (
+          ""
+        )
+      } <
+      span className = "congrats__text" > {
+        this.state.selectedRestaurant.address
+      } <
+      /span> <
+    span className = "congrats__text" > {
+        this.state.selectedRestaurant.city
+      }, {
+        " "
+      } {
+        this.state.selectedRestaurant.state
+      } <
+      /span> <
+    button className = "congrats__button"
+    onClick = {
+        () => {
+          this.handlePopupClose();
+        }
+      } >
+      Lets Go!
+      <
+      /button> < /
+    ReactModal > <
+      /React.Fragment>
+  );
+}
 }
 
 const mapStateToProps = state => {
