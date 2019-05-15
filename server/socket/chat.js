f6223895ad3fa4b3bc8515b6dd726e50218d2b66
Omar Juice <ojameer1@gmail.com>
2019-05-14 21:06:13 -0400

    app cache functioning

--- server/socket/chat.js
+++ server/socket/chat.js
@@ -1,89 +1,5 @@
-const { User } = require("../db/models");
 const cache = require("./appCache");
 
-// const allChats = {
-//   // roomId: [
-//   //   [msg, id],
-//   //   [msg, id]
-//   // ]
-// };
-
-// const roomInfo = {
-//   // user: {
-//   //   matchId,
-//   //   roomId,
-//   //   socketId
-//   // }
-// };
-
-// const venueList = {
-//   //user1: [ resId ],
-//   //user2: [ resId ]
-// };
-
-// let roomId = 0;
-
-// function getChatHistory(roomId) {
-//   console.log("getCHatHistory - roomId", roomId);
-//   if (allChats[roomId] === undefined) {
-//     allChats[roomId] = [];
-//   }
-//   return allChats[roomId];
-// }
-
-// function clearRecord(userId, roomId) {
-//   delete allChats[roomId];
-//   delete roomInfo[userId];
-// }
-
-// const checkMatchId = async (socket, userId) => {
-//   //get matchId from database
-//   const { id } = socket;
-//   const userInfo = await User.findByPk(userId);
-//   console.log("userInfo", userInfo);
-//   const matchId = userInfo.hasMatched;
-//   console.log("matchId", matchId);
-//   // check if your match already has a room
-//   if (roomInfo.hasOwnProperty(matchId)) {
-//     roomInfo[userId] = {
-//       matchId: matchId,
-//       id: id,
-//       roomId: roomInfo[matchId].roomId
-//     };
-//   } else {
-//     roomInfo[userId] = {
-//       matchId: matchId,
-//       id: id,
-//       roomId: String(roomId)
-//     };
-//     roomId++;
-//   }
-//   console.log("chatMatchId - roomInfo", roomInfo);
-// };
-
-// function addNewMessage(userId, msg) {
-//   let roomName = roomInfo[userId].roomId;
-//   if (allChats[roomName] === []) {
-//     allChats[roomName] = [[msg, userId]];
-//   } else {
-//     allChats[roomName].push([msg, userId]);
-//   }
-//   console.log("addNewMessage - allchats", allChats);
-// }
-
-// function createVenueList(userId) {
-//   venueList[userId] = [];
-// }
-
-// function checkVenueList(userId, restaurantId) {
-//   const matchId = roomInfo[userId].matchId;
-//   if (venueList[matchId].includes(restaurantId)) {
-//     return true;
-//   } else {
-//     return false;
-//   }
-// }
-//socket
 module.exports = function(socket, userId) {
   //joining chatroom and sending back chat history
   socket.on("join-chatroom", async () => {
@@ -102,7 +18,7 @@ module.exports = function(socket, userId) {
   socket.on("send-client-message", msg => {
     try {
       cache.addMessage(msg, userId);
-      console.log(cache.getChatHistory(userId));
+      // console.log(cache.getChatHistory(userId));
       socket.broadcast.to(cache.getRoom(userId)).emit("send-others-message", {
         msg,
         userId
@@ -142,8 +58,9 @@ module.exports = function(socket, userId) {
       cache.venueList[userId].push(restaurantId);
       if (cache.checkVenueList(userId, restaurantId)) {
         socket.to(cache.getRoom(userId)).emit("matched", restaurantId);
-        console.log("resId in chat backend", restaurantId);
-        console.log("venue list", cache.venueList);
+        socket.emit("matched", restaurantId);
+        // console.log("resId in chat backend", restaurantId);
+        // console.log("venue list", cache.venueList);
       }
     } catch (e) {
       console.log(e);
