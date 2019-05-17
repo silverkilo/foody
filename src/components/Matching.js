d3a7bbae2f48467c5f589f183544c6163166868d
Omar Juice <ojameer1@gmail.com>
2019-05-17 11:51:54 -0400

    change

--- src/components/Matching.js
+++ src/components/Matching.js
@@ -40,7 +40,6 @@ class Matching extends Component {
           {" "}
           {this.props.loading ? null : <MatchStack users={users} />}{" "}
         </div>{" "}
-
         {this.props.didMatch.matched ? (
           <ReactModal
             isOpen={this.props.didMatch.matched ? true : false}
@@ -49,18 +48,18 @@ class Matching extends Component {
             contentLabel="Restaurant Selected Modal"
             className="congrats__content"
             overlayClassName="congrats__overlay"
-          // style={{ overlay: {}, content: "hi is this working" }}
-          // portalClassName="ReactModalPortal"
-          // overlayClassName="ReactModal__Overlay"
-          // className="ReactModal__Content"
-          // bodyOpenClassName="ReactModal__Body--open"
-          // htmlOpenClassName="ReactModal__Html--open"
-          // ariaHideApp={true}
-          // role="dialog"
-          // parentSelector={() => document.body}
-          // data={{
-          //   background: "blue"
-          // }}
+            // style={{ overlay: {}, content: "hi is this working" }}
+            // portalClassName="ReactModalPortal"
+            // overlayClassName="ReactModal__Overlay"
+            // className="ReactModal__Content"
+            // bodyOpenClassName="ReactModal__Body--open"
+            // htmlOpenClassName="ReactModal__Html--open"
+            // ariaHideApp={true}
+            // role="dialog"
+            // parentSelector={() => document.body}
+            // data={{
+            //   background: "blue"
+            // }}
           >
             <i className="fas fa-user-friends congrats__icon" />
             <h1 className="congrats__title"> Congratulations! </h1>{" "}
